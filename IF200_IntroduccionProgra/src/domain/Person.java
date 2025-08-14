d7812155ddd093830e8075dcf1622cfb671b8602
delaoariana396 <delaoariana396@gmail.com>
2025-08-14 11:25:39 -0600

    Mi primer commit en Java

---
+++ IF200_IntroduccionProgra/src/domain/Person.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+
+public class Person {
+    
+    //atributos globales (no estan dentro del main) se van a poder usar en mi clase. 
+    private String name;
+    private String lastName;
+    private String id;
+    private String phone;
+    private int age;
+
+    //contrl + alt + insert para generar el constructor automaticamente
+    public Person(String name, String lastName, String id, String phone, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.id = id;
+        this.phone = phone;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+// salidas, igual se generan con contrl alt 
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", lastName=" + lastName + ", id=" + id + ", phone=" + phone + ", age=" + age + '}';
+    }
+    
+    
+}
---
+++ IF200_IntroduccionProgra/src/if200_introduccionprogra/IF200_IntroduccionProgra.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package if200_introduccionprogra;
+
+/**
+ *
+ * @author Dell
+ */
+public class IF200_IntroduccionProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    
+    String name = "Lionel";
+    int age = 40;
+    /* Tipos de datos numéricos:
+    int, long,short, float, double,numeric
+    money, decimal
+    
+    
+    Manejo de fechas: 
+    Date 
+    
+    cadena de caracteres: 
+    String 
+    
+    objetos: 
+    
+    object
+    
+    Data Structures: Arrays or Vectors: int[] x;
+    int [][] y;
+    */
+    System.out.println("Hello World");
+    System.out.println("Name:" + name);
+    System.out.println("Age:" + age);
+    
+}
+}
